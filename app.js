d4c399b60a0982d6fdeadc49012c5c2186a87415
acmcalvo <91921941+acmcalvo@users.noreply.github.com>
2021-12-09 23:45:38 -0500

    create dis change path of of index created style.css

--- app.js
+++ app.js
@@ -131,9 +131,24 @@ promptUser()
   .then(portfolioData => {
     const pageHTML = generatePage(portfolioData);
 
-    fs.writeFile('./index.html', pageHTML, err => {
-      if (err) throw new Error(err);
-
-      console.log('Page created! Check out index.html in this directory to see it!');
-    });
+    // change the path to look like this
+    promptUser()
+  .then(promptProject)
+  .then(portfolioData => {
+    return generatePage(portfolioData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
   });
+  });
+  
